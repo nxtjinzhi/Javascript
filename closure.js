1b5b05963f27162e2244a40a2b9b27d90f3401b8
Jinzhi Chen <nxtjinzhi@gmail.com>
2015-09-24 15:23:46 +0800

    add Closure specification, part1
    add `let` and `const` specification, which support block-devel scope
    
    Signed-off-by: Jinzhi Chen <nxtjinzhi@gmail.com>

--- closure.js
+++ closure.js
@@ -1,10 +1,14 @@
+'use strict'
 // `let` and `const`
+// ES6 introduces two new variable types, `let` and `const`, both of which allow developers to use block-level scope.
 var foo = "node";
 console.log("outside foo: " +foo);
 for (var  x = 0; x < 1; x++) {
-	var foo = "bar";
+	let foo = "bar";
 	console.log("inner foo: " +foo);
 }
+// * note the difference with the example in the `function.js`
+// the `let` doesn't cause global namespace pollution
 console.log("outside foo: "+foo);
 
 var a = 5;
